b911d62c3501532d45bd9e30d2d2405e1302e097
Jerop <jerop@google.com>
2021-05-21 12:07:41 -0500

    Hello World

--- hello.go
+++ hello.go
@@ -7,5 +7,5 @@ func Hello(name string) string {
 }
 
 func main() {
-	fmt.Println(Hello("Kenya"))
+	fmt.Println(Hello("World"))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	got := Hello("Kenya")
-	want := "Hello Kenya!"
+	want := "Hello World!"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
